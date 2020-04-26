29ce9321da7abfa8659e4593ec1a9ca03d4da691
Vika <viktoriav.aero@gmail.com>
2020-04-26 17:26:59 +0300

    HW 6 new

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,1 +1,16 @@
-// Your code goes here
+'use strict';
+const num = Number.parseFloat(prompt('Input check number', ''));
+const tip = Number.parseFloat(prompt('Input tip percentage', ''));
+const hundred = 100;
+const tipAmount = Math.round(num * tip) / hundred;
+const total = num + tipAmount;
+
+if ( typeof num === 'number' && num >= 0 && typeof tip === 'number' && tip >= 0 && tip < hundred && total >= 0) {
+    alert(`
+    Check number:  ${num}
+    Tip: ${tip} %
+    Tip amount: ${tipAmount}
+    Total sum to pay: ${total}`);
+} else {
+    alert('Invalid input data');
+}
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,21 @@
-// Your code goes here
+'use strict';
+const word = prompt('Please, enter the word', '');
+
+if (typeof word === 'string') {
+    const wordTrim = word.trim();
+    const wordLength = word.length;
+    const mod = 2;
+    const k = Math.floor(wordLength / mod);
+    let num;
+    if (wordTrim.length === 0) {
+        alert('Invalid value');
+    } else if (wordLength % mod === 0) {
+        num = k;
+        alert(word.substring(num - 1, num + 1));
+    } else {
+        num = k;
+        alert(word.slice(num, length - num));
+    } 
+} else {
+        alert('Invalid value');
+    }
